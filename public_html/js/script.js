7ead74115193bf44f59dc9964627f9166766f36a
kacperente <kacper.gala@wp.pl>
2016-11-06 13:43:18 +0100

    Navbar, Projects, Images
    Fix bugs and features

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    $(window).scroll(function() { // check if scroll event happened
+        if ($(document).scrollTop() > 1) { // check if user scrolled more than 50 from top of the browser window
+            $(".navbar-inverse").addClass("scroll"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+        } else {
+            $(".navbar-inverse").removeClass("scroll"); // if not, change it back to transparent
+        }
+    });
     $(".burger-menu").hide();
     $(".burger").click(function() {
         if ($(".burger-menu").is(":hidden")) {
@@ -25,6 +32,12 @@ $(document).ready(function(){
             $(".burger-menu").slideUp('fast');
         }
         $.scrollTo($('#projects'),500);
+    });
+    $('.menu-contact').click(function(){
+        if($(".burger-menu").is(":visible")){
+            $(".burger-menu").slideUp('fast');
+        }
+        $.scrollTo($('#contact'),500);
     });
 });
 
